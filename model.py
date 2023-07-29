Several improvements and fixes - Fix an issue in which the AUC calculation was not working due to using minibatches. Now it is computed directly on the whole validation and test subsets. - Remove batch normalization (use layer normalization). - Change SGD to AdamW. - Add early stopping. - Add an option to use a seed and deterministic algorithms for reproducibility. - Add code for hyperparameter search with wandb. - Update .gitignore.

model.py
@@ -35,8 +35,8 @@ class Classifier(nn.Module):
     num_layers : int
         number of layers in the MLP
 
-    norm : str | None
-        normalization to use after each layer. One of None, 'batch', 'layer'. If None, no normalization
+    layer_norm : bool
+        whether to use layer normalization
 
     hidden_size : int | None
         size of hidden layers. If None, hidden size is equal to the input size
@@ -48,12 +48,12 @@ class Classifier(nn.Module):
         the features are normalized to unit length and then divided by this value
     """
 
-    def __init__(self, in_features, out_features, num_layers=1, normalization=None, hidden_size=None, skip_every=None, temperature=0.05):
+    def __init__(self, in_features, out_features, num_layers=1, layer_norm=True, hidden_size=None, skip_every=None, temperature=0.05):
         super().__init__()
 
         self.num_layers = num_layers
         self.skip_every = skip_every if skip_every is not None else num_layers + 1
-        self.norm = normalization
+        self.layer_norm = layer_norm
         self.temperature = temperature
 
         self.mlp = nn.ModuleList()
@@ -64,16 +64,12 @@ class Classifier(nn.Module):
                 hidden_size = in_features
             self.mlp.append(nn.Linear(in_features, hidden_size))
             self.mlp.append(nn.GELU())
-            if normalization == 'batch':
-                self.mlp.append(nn.BatchNorm1d(hidden_size))
-            elif normalization == 'layer':
+            if layer_norm:
                 self.mlp.append(nn.LayerNorm(hidden_size))
             for _ in range(num_layers - 2):
                 self.mlp.append(nn.Linear(hidden_size, hidden_size))
                 self.mlp.append(nn.GELU())
-                if normalization == 'batch':
-                    self.mlp.append(nn.BatchNorm1d(hidden_size))
-                elif normalization == 'layer':
+                if layer_norm:
                     self.mlp.append(nn.LayerNorm(hidden_size))
             self.mlp.append(nn.Linear(hidden_size, out_features))
 
@@ -92,7 +88,7 @@ class Classifier(nn.Module):
             skip = x
             for i, layer in enumerate(self.mlp[1:-1]):
                 x = layer(x)
-                if (i - 1) % ((2 + (1 if self.norm else 0)) * self.skip_every) == 0:
+                if (i - 1) % ((2 + int(self.layer_norm)) * self.skip_every) == 0:
                     x = x + skip
                     skip = x
         # (batch_size, hidden_size)
@@ -127,7 +123,8 @@ class ProtoClassifier(nn.Module):
         center = torch.nan_to_num(torch.vstack([t_feat[label == i].mean(dim=0) for i in range(self.num_classes)]))
         invalid_idx = center.sum(dim=1) == 0
         if invalid_idx.any() and self.label is not None:
-            old_center = torch.vstack([t_feat[self.label == i].mean(dim=0) for i in range(self.num_classes)])
+            print("Warning: invalid center(s) found, using old center(s) instead", flush=True)
+            old_center = torch.nan_to_num(torch.vstack([t_feat[self.label == i].mean(dim=0) for i in range(self.num_classes)]))
             center[invalid_idx] = old_center[invalid_idx]
         else:
             self.label = label
@@ -141,15 +138,15 @@ class ProtoClassifier(nn.Module):
 
 
 class Model(nn.Module):
-    def __init__(self, in_features, out_features, num_layers=1, normalization=True, hidden_size=None, skip_every=None, temperature=0.05):
+    def __init__(self, in_features, out_features, num_layers=1, layer_norm=True, hidden_size=None, skip_every=None, temperature=0.05):
         super().__init__()
 
-        self.classifier = Classifier(in_features, out_features, num_layers, normalization, hidden_size, skip_every, temperature)
+        self.classifier = Classifier(in_features, out_features, num_layers, layer_norm, hidden_size, skip_every, temperature)
 
         self.criterion = nn.CrossEntropyLoss(reduction='none')
 
     def forward(self, x, reverse=False, lamda=0.1):
-        return self.classifier(x, reverse=False, lamda=0.1)
+        return self.classifier(x, reverse=reverse, lamda=lamda)
 
     def get_features(self, x, reverse=False, lamda=0.1):
         return self.classifier.get_features(x, reverse=reverse, lamda=lamda)

trainer.py
@@ -34,7 +34,7 @@ class BaseTrainer:
     def __init__(self, model: Model, data_loaders: DataLoaders, lr=1e-3, num_iters=10000, **kwargs):
         self.model = model
         self.data_loaders = data_loaders
-        self.optimizer = torch.optim.SGD(self.model.parameters(), lr=lr, momentum=0.9, weight_decay=5e-4, nesterov=True)
+        self.optimizer = torch.optim.AdamW(self.model.parameters(), lr=lr)
         self.lr_scheduler = LearningRateScheduler(self.optimizer, num_iters)
         self.num_iters = num_iters
 
@@ -58,7 +58,7 @@ class BaseTrainer:
                     y_pred = F.sigmoid(out[:, 1])
                 else:
                     y_pred = F.softmax(out, dim=1)
-                auc = metric(y_pred, y)
+                metric.update(y_pred, y)
         auc = metric.compute()
         return auc
 
@@ -81,8 +81,9 @@ class BaseTrainer:
 
         return s_loss.item(), t_loss.item(), 0
 
-    def train(self, eval_interval):
+    def train(self, eval_interval, early_stop=None):
         best_val_auc = 0.0
+        iterations_without_improvement = 0
         iter_train_data_loaders = iter(self.data_loaders)
         self.model.train()
         start_time = time.time()
@@ -90,6 +91,7 @@ class BaseTrainer:
         for step in range(1, self.num_iters + 1):
             print(f"Step {step}", flush=True)
             (sx, sy), (tx, ty), ux = next(iter_train_data_loaders)
+            # with torch.autograd.detect_anomaly():  # for debugging the next line
             s_loss, t_loss, u_loss = self.training_step(step, sx, sy, tx, ty, ux)
             self.lr_scheduler.step()
 
@@ -98,9 +100,8 @@ class BaseTrainer:
                 't_loss': t_loss,
                 'u_loss': u_loss,
                 'lr': self.lr_scheduler.get_lr(),
-                'step': step,
                 'time': (time.time() - start_time) / 60,
-            })
+            }, step=step)
 
             if step % eval_interval == 0 or step == self.num_iters:
                 print("Evaluating", flush=True)
@@ -108,12 +109,18 @@ class BaseTrainer:
                 wandb.log({
                     'val_auc': val_auc,
                     'test_auc': test_auc,
-                    'step': step
-                })
+                }, step=step)
                 if val_auc > best_val_auc:
                     best_val_auc = val_auc
+                    iterations_without_improvement = 0
                     if wandb.run:
+                        wandb.run.summary['best_val_auc'] = val_auc
                         wandb.run.summary['best_test_auc'] = test_auc
+                else:
+                    iterations_without_improvement += eval_interval
+                    if early_stop is not None and iterations_without_improvement >= early_stop:
+                        print("Early stopping", flush=True)
+                        break
 
 
 class UnlabeledTrainer(BaseTrainer):